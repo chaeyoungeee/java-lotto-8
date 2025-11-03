80008d7821c7f109d3ec97a70093d3ed05604b2b
chaeyoungeee <chaeinge@gmail.com>
2025-11-03 17:45:12 +0900

    feat(OutputView): 출력 기능 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,47 @@
+package lotto.view;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.enums.LottoPrize;
+
+public class OutputView {
+
+    private final String PURCHASE_RESULT_MESSAGE = "%d개를 구매했습니다.";
+    private final String RESULT_MESSAGE = "당첨 통계\n---";
+    private final String RESULT_MATCH_COUNT_MESSAGE = "%d개 일치";
+    private final String RESULT_IS_BONUS_MATCH_MESSAGE = ", 보너스 볼 일치";
+    private final String RESULT_AMOUNT_MESSAGE = " (%,d원) - %d개\n";
+    private final String RETURN_RATE_MESSAGE = "총 수익률은 %.2f%%입니다.";
+
+
+    public void printPurchaseResult(int count, List<List<Integer>> lottos) {
+        System.out.printf(PURCHASE_RESULT_MESSAGE + "\n", count);
+        for (List<Integer> lotto : lottos) {
+            System.out.println(lotto.stream().sorted().toList());
+        }
+    }
+
+    public void printResult(Map<LottoPrize, Integer> results) {
+//       5개 일치, 보너스 볼 일치 (30,000,000원) - 0개
+        System.out.println(RESULT_MESSAGE);
+        Arrays.stream(LottoPrize.values())
+            .filter(prize -> prize != LottoPrize.NONE)
+            .forEach(prize -> {
+                int count = results.getOrDefault(prize, 0);
+                System.out.printf(RESULT_MATCH_COUNT_MESSAGE, prize.getMatchCount());
+                if (prize.isBonusMatch()) {
+                    System.out.print(RESULT_IS_BONUS_MATCH_MESSAGE);
+                }
+                System.out.printf(RESULT_AMOUNT_MESSAGE, prize.getAmount(), count);
+            });
+    }
+
+    public void printReturnRate(double rate) {
+        System.out.printf(RETURN_RATE_MESSAGE + "\n", rate);
+    }
+
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
+}
