e97d52ea3ebb979b2ab366e80ad03e0a3cf4733f
chaeyoungeee <chaeinge@gmail.com>
2025-11-03 17:51:41 +0900

    feat(LottoController): 로또 애플리케이션 실행 로직 연결

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,8 +1,16 @@
 package lotto.controller;
 
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
 import lotto.domain.LottoFactory;
-import lotto.domain.LottoGame;
+import lotto.domain.PurchaseAmount;
+import lotto.domain.WinningCombination;
+import lotto.domain.enums.LottoPrize;
+import lotto.dto.LottoResultDto;
+import lotto.dto.MatchCountDto;
 import lotto.service.LottoService;
+import lotto.util.RandomLottoNumberGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -10,17 +18,40 @@ public class LottoController {
 
     private final InputView inputView;
     private final OutputView outputView;
-    private final LottoService lottoService = new LottoService();
+    private final RandomLottoNumberGenerator randomLottoNumberGenerator;
 
-    public LottoController(InputView inputView, OutputView outputView) {
-        this.inputView = new InputView();
-        this.outputView = new OutputView();
+    public LottoController(InputView inputView, OutputView outputView, RandomLottoNumberGenerator randomLottoNumberGenerator) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.randomLottoNumberGenerator = randomLottoNumberGenerator;
     }
 
-    public void start() {
-        LottoGame lottoGame = new LottoGame(
-                LottoFactory.createPurchaseAmount(inputView, outputView),
-                LottoFactory.createWinningCombination(inputView, outputView)
-        );
+    public void run() {
+        LottoService lottoService = new LottoService(randomLottoNumberGenerator);
+        PurchaseAmount purchaseAmount = LottoFactory.createPurchaseAmount(inputView, outputView);
+        List<Lotto> lottos = generateLottos(lottoService, purchaseAmount);
+        WinningCombination winningCombination = LottoFactory.createWinningCombination(inputView, outputView);
+        calculateResult(lottoService, lottos, winningCombination, purchaseAmount);
+    }
+
+    private List<Lotto> generateLottos(LottoService lottoService, PurchaseAmount purchaseAmount) {
+        List<Lotto> lottos = lottoService.generateLottos(purchaseAmount);
+        outputView.printPurchaseResult(purchaseAmount.getLottoCount(), toNumberList(lottos));
+        return lottos;
+    }
+
+    private void calculateResult(LottoService lottoService, List<Lotto> lottos, WinningCombination winningCombination, PurchaseAmount purchaseAmount) {
+        List<MatchCountDto> matchCounts = lottoService.evaluateLottos(lottos, winningCombination);
+        LottoResultDto results = lottoService.getLottoResults(matchCounts);
+        Map<LottoPrize, Integer> prizeCountMap = results.getPrizeCountMap();
+        outputView.printResult(prizeCountMap);
+        double returnRate = lottoService.calculateReturnRate(results, purchaseAmount);
+        outputView.printReturnRate(returnRate);
+    }
+
+    private static List<List<Integer>> toNumberList(List<Lotto> lottos) {
+        return lottos.stream()
+                .map(Lotto::getNumbers)
+                .toList();
     }
 }
